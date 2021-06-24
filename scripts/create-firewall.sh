5dbe56b04b7c8203dfab83a2f56dc5232ee15fc4
whatasoda <git@whatasoda.me>
2021-06-25 01:31:42 +0900

    fix: firewall creation script

--- scripts/create-firewall.sh
+++ scripts/create-firewall.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+if [ "$GOOGLE_CLOUD_PROJECT" == '' ]; then
+  echo "Please set \$GOOGLE_CLOUD_PROJECT"
+  exit 1
+fi
+
 gcloud config set core/project $GOOGLE_CLOUD_PROJECT
 
 result=0
 gcloud compute firewall-rules list --format='value(name)' | grep -q ^mcs$ || result=$?
 if [ "$result" != "0" ]; then
-  gcloud compute firewall-rules create mcs
+  gcloud compute firewall-rules create mcs \
     --target-tags=minecraft-server \
     --direction=IN \
     --allow='tcp:25565' \
@@ -17,7 +22,7 @@ fi
 result=0
 gcloud compute firewall-rules list --format='value(name)' | grep -q ^mcs-dev$ || result=$?
 if [ "$result" != "0" ]; then
-  gcloud compute firewall-rules create mcs-dev
+  gcloud compute firewall-rules create mcs-dev \
     --target-tags=minecraft-server-dev \
     --direction=IN \
     --allow='tcp:8000' \
