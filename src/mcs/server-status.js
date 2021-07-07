f57b53cf585e220687a49e51b62956276327dccb
whatasoda <git@whatasoda.me>
2021-07-07 23:36:28 +0900

    feat: filter server process info

--- src/mcs/server-status.js
+++ src/mcs/server-status.js
@@ -1,7 +1,16 @@
 require('minecraft-server-util')
   .statusFE01('localhost')
   .then((res) => {
-    process.stdout.write(JSON.stringify(res));
+    const { description, version, modInfo, maxPlayers, onlinePlayers } = res;
+    /** @type {import('../types/meteora').default.ServerProcessInfo} */
+    const info = {
+      description: description.toString(),
+      version,
+      modInfo,
+      maxPlayers,
+      onlinePlayers,
+    };
+    process.stdout.write(JSON.stringify(info));
     process.exit(0);
   })
   .catch((err) => {
--- src/types/meteora.ts
+++ src/types/meteora.ts
@@ -42,3 +42,5 @@ declare global {
     }
   }
 }
+
+export default Meteora;
