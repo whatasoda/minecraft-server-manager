16e0f101511d1fe69b3a25111ad23f90b326fcb7
whatasoda <git@whatasoda.me>
2021-07-09 00:14:56 +0900

    feat: add error toast to mcs client service

--- src/client/contexts/services.tsx
+++ src/client/contexts/services.tsx
@@ -1,6 +1,4 @@
 import React, { createContext, useContext, useMemo } from 'react';
-import ResponseResult from '../../shared/responseResult';
-import toast from '../components/_overlays/toast';
 import authService from '../services/auth';
 import mcsService from '../services/mcs';
 import userService from '../services/user';
@@ -18,49 +16,6 @@ export const ServiceProvider: React.FC = ({ children }) => {
       auth: authService,
       mcs: {
         ...mcsService,
-        create: injectResultToast(
-          mcsService.create,
-          () => {
-            toast.success('Machine successfully created');
-          },
-          (_, message) => {
-            toast.danger(`Failed to create due to "${message}"`);
-          },
-        ),
-        start: injectResultToast(
-          mcsService.start,
-          () => {
-            toast.success('Machine successfully started');
-          },
-          (_, message) => {
-            toast.danger(`Failed to start machine due to "${message}"`);
-          },
-        ),
-        stop: injectResultToast(
-          mcsService.stop,
-          () => {
-            toast.success('Machine successfully stopped');
-          },
-          (_, message) => {
-            toast.danger(`Failed to stop machine due to "${message}"`);
-          },
-        ),
-        delete: injectResultToast(
-          mcsService.delete,
-          () => {
-            toast.success('Machine successfully deleted');
-          },
-          (_, message) => {
-            toast.danger(`Failed to delete machine due to "${message}"`);
-          },
-        ),
-        status: injectResultToast(
-          mcsService.status,
-          () => {},
-          (_, message) => {
-            toast.danger(`Failed to retrieve machine info due to "${message}"`);
-          },
-        ),
       },
       user: {
         ...userService,
@@ -69,21 +24,6 @@ export const ServiceProvider: React.FC = ({ children }) => {
   }, []);
 
   return <ServiceContext.Provider value={value} children={children} />;
-  function injectResultToast<T extends {}, U extends any[]>(
-    fn: (...args: U) => Promise<ResponseResult.Result<T>>,
-    success: (data: T) => void,
-    error: (status: number, message: string) => void,
-  ): typeof fn {
-    return async (...args: U) => {
-      const result = await fn(...args);
-      if (result.error === null) {
-        success(result.data);
-      } else {
-        error(result.error.status, result.error.message);
-      }
-      return result;
-    };
-  }
 };
 
 export const useServices = () => {
--- src/client/services/mcs.ts
+++ src/client/services/mcs.ts
@@ -1,43 +1,81 @@
 import type { McsHandlers as McsServerHandlers } from '../../server/apis/mcs';
 import type { McsHandlers as McsInstanceHandlers } from '../../mcs';
 import createApiClient from '../../shared/apiClientFactory';
+import toast from '../components/_overlays/toast';
 
-const mcsService = createApiClient<McsServerHandlers & Omit<McsInstanceHandlers, '/status'>>({ baseURL: '/api/mcs' })({
+const baseURL = '/api/mcs';
+
+const failGet = (path: string, message: string) => {
+  return `Failed to get '${baseURL}${path}' due to: '${message}'`;
+};
+
+const failPost = (path: string, message: string) => {
+  return `Failed to call '${baseURL}${path}' due to: '${message}'`;
+};
+
+const mcsService = createApiClient<McsServerHandlers & Omit<McsInstanceHandlers, '/status'>>({ baseURL })({
   list: {
     path: '/list',
     method: 'get',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failGet('/list', error?.message));
+    },
   },
   log: {
     path: '/log',
     method: 'get',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failGet('/log', error?.message));
+    },
   },
   status: {
     path: '/status',
     method: 'get',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failGet('/status', error?.message));
+    },
   },
   operation: {
     path: '/operation',
     method: 'get',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failGet('/operation', error?.message));
+    },
   },
   create: {
     path: '/create',
     method: 'post',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failPost('/create', error?.message));
+    },
   },
   start: {
     path: '/start',
     method: 'post',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failPost('/start', error?.message));
+    },
   },
   stop: {
     path: '/stop',
     method: 'post',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failPost('/stop', error?.message));
+    },
   },
   delete: {
     path: '/delete',
     method: 'post',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failPost('/delete', error?.message));
+    },
   },
   dispatch: {
     path: '/make',
     method: 'post',
+    onResponse: ({ error }) => {
+      error?.message && toast.danger(failPost('/make', error?.message));
+    },
   },
 });
 
